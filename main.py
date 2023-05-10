f07d2817666f0bb51a6ccd8eb34cecf8a93fa7fd
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-05-10 23:02:47 +0500

    Edited second text block

--- main.py
+++ main.py
@@ -27,18 +27,24 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
 
         self.sections = MainButtons(self, ("100 words", test), ("1000 words", test), ("2000 words", test),
-                                     ("3000 words", test))
+                                    ("3000 words", test))
         self.sections.grid(row=0, column=0, padx=10, pady=(40, 20), sticky="nsw")
 
         self.progressbar = customtkinter.CTkProgressBar(self, orientation="horizontal")
         self.progressbar.grid(row=0, column=0, padx=10, pady=(15, 15), sticky="nw")
 
-        self.textbox = customtkinter.CTkTextbox(master=self, width=600, corner_radius=0, fg_color="white")
-        self.textbox.grid(row=0, column=0, padx=200, pady=50, sticky="nsew", columnspan=2)
+        self.textbox = customtkinter.CTkTextbox(master=self, corner_radius=0, fg_color="white",
+                                                font=("calibri", 40), text_color="black")
+        self.textbox.grid(row=0, column=0, padx=(200, 10), pady=(50, 10), sticky="NWES", columnspan=2, rowspan=2)
+        self.textbox.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
         self.textbox.insert("0.0",
-                            "Добро пожаловать. Тебе нужно выбрать уровень чтобы приступить к обучению. Для начала"
-                            "лучше освоить самые базовые слова, чтобы перейти к следующим. Однако слова будут"
-                            "дублироваться от уровня к уровню. Так основы основ гораздо лучше заркепяться в голове")
+                            "Добро пожаловать\n"
+                            "Тебе нужно выбрать уровень чтобы приступить к обучению\n"
+                            "Для начала лучше освоить самые базовые слова, "
+                            "чтобы перейти к следующим\n"
+                            "Однако слова будут дублироваться от уровня к уровню. "
+                            "Так основы основ гораздо лучше заркепяться в голове")
 
 
 if __name__ == "__main__":
