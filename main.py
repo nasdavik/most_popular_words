ba042530055775de27765718cf126523363fe06e
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-05-12 00:47:48 +0500

    started rewriting the structure

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import customtkinter
 
 
-def test():
-    print("yeah, all right")
-
-
 class MainButtons(customtkinter.CTkFrame):
+    # Фрейм для основных кнопок. При нажатии должен меняться self.textbox
     def __init__(self, master, *args):
         super().__init__(master)
         self.buttons = []
 
         for i, value in enumerate(args):
             button = customtkinter.CTkButton(self, text=value[0], command=value[1])
-            button.grid(row=i, column=0, padx=10, pady=10)
+            button.grid(row=i, column=0, padx=15, pady=(20, 0))
             self.buttons.append(button)
 
 
-class App(customtkinter.CTk):
+class FunctionsMixin:
+    @staticmethod
+    def test():
+        print("yeah, all right")
+
+
+class App(customtkinter.CTk, FunctionsMixin):
     def __init__(self):
         super().__init__(fg_color="white")
 
@@ -26,12 +29,14 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.sections = MainButtons(self, ("100 words", test), ("1000 words", test), ("2000 words", test),
-                                    ("3000 words", test))
-        self.sections.grid(row=0, column=0, padx=10, pady=(40, 20), sticky="nsw")
+        self.sections = MainButtons(self, ("100 words", self.test), ("1000 words", self.test),
+                                    ("2000 words", self.test), ("3000 words", self.test),
+                                    ("5000 words", self.test), ("7000 words", self.test),
+                                    ("10000 words", self.test), ("20000 words", self.test))
+        self.sections.grid(row=0, column=0, padx=15, pady=(50, 20), sticky="nsw")
 
         self.progressbar = customtkinter.CTkProgressBar(self, orientation="horizontal")
-        self.progressbar.grid(row=0, column=0, padx=10, pady=(15, 15), sticky="nw")
+        self.progressbar.grid(row=0, column=0, padx=15, pady=(15, 15), sticky="nw")
 
         self.textbox = customtkinter.CTkTextbox(master=self, corner_radius=0, fg_color="white",
                                                 font=("calibri", 40), text_color="black")
