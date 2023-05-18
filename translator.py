b3f0687bb29d8b8044147b1a9dcd035ad88798cb
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-05-18 23:45:22 +0500

    Trying pars yandex-translator

--- translator.py
+++ translator.py
@@ -5,8 +5,9 @@ word = "hello"
 url = "https://translate.yandex.ru/?source_lang=en&target_lang=ru&text=" + word
 
 response = requests.get(url)
+print(response)
 soup = BeautifulSoup(response.content, "html.parser")
 
 answer = []
-print(soup.select(".trnslator-container > .translation-word > .translation-chunk"))
+print(soup.find(class_="textinput"))
 
