e81a3c7cb702617327f5264fd3b566886153622f
nasdavik <sew542@mail.ru>
2023-05-30 18:07:32 +0500

    Start use selenium for parsing

--- translator.py
+++ translator.py
@@ -1,4 +1,15 @@
-from nltk.corpus import wordnet
+from selenium import webdriver
+import time
 
+url = "https://translate.google.com/"
+driver: webdriver = webdriver.Chrome(executable_path=
+                    "C:\\Users\\1\\PycharmProjects\\most_popular_words\\chromdriver\\oldversion\\chromedriver.exe")
 
-print(wordnet.synonyms("select"))
+try:
+    driver.get(url=url)
+    time.sleep(5)
+except Exception as ex:
+    print(ex)
+finally:
+    driver.close()
+    driver.quit()
