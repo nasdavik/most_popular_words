72687f3d7654f95b3de291aff30a58f764575d17
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-05-30 23:15:46 +0500

    Update file with selenium

--- translator.py
+++ translator.py
@@ -2,12 +2,15 @@ from selenium import webdriver
 import time
 
 url = "https://translate.google.com/"
-driver: webdriver = webdriver.Chrome(executable_path=
-                    "C:\\Users\\1\\PycharmProjects\\most_popular_words\\chromdriver\\oldversion\\chromedriver.exe")
+# driver: webdriver = webdriver.Chrome(executable_path=
+#                     "C:\\Users\\1\\PycharmProjects\\most_popular_words\\chromdriver\\oldversion\\chromedriver.exe")
+
+driver: webdriver = webdriver.Firefox(executable_path=
+                                      "C:\\Пользователи\\Данила\\PycharmProjects\\most_popular_words\\fierfoxdriver\\geckodriver-0.33.0")
 
 try:
-    driver.get(url=url)
-    time.sleep(5)
+    driver.get(url="https://youtube.com")
+    time.sleep(10)
 except Exception as ex:
     print(ex)
 finally:
