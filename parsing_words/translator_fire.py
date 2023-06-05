8a78b06093ee503c61247ef404a41d89b4b1ec57
nasdavik <sew542@mail.ru>
2023-06-05 13:41:13 +0500

    Updated translator and created parsing dirictiry

--- parsing_words/translator_fire.py
+++ parsing_words/translator_fire.py
@@ -1,15 +1,16 @@
 from selenium import webdriver
 import time
+from fake_useragent import UserAgent
 
+useragent: UserAgent = UserAgent()
+options: webdriver = webdriver.FirefoxOptions()
+options.set_preference("general.useragent.override", useragent.random)
 url = "https://translate.google.com/"
-# driver: webdriver = webdriver.Chrome(executable_path=
-#                     "C:\\Users\\1\\PycharmProjects\\most_popular_words\\chromdriver\\oldversion\\chromedriver.exe")
-
 driver: webdriver = webdriver.Firefox(executable_path=
                                       "C:\\Пользователи\\Данила\\PycharmProjects\\most_popular_words\\fierfoxdriver\\geckodriver-0.33.0")
 
 try:
-    driver.get(url="https://youtube.com")
+    driver.get(url=url)
     time.sleep(10)
 except Exception as ex:
     print(ex)
