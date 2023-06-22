5596838b8b21bc64d85dfabcb987d77359423e66
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-06-23 00:02:50 +0500

    Change somthing...

--- translator.py
+++ translator.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import time
 
+
 url = "https://translate.google.com/"
 # driver: webdriver = webdriver.Chrome(executable_path=
 #                     "C:\\Users\\1\\PycharmProjects\\most_popular_words\\chromdriver\\oldversion\\chromedriver.exe")
@@ -8,9 +9,12 @@ url = "https://translate.google.com/"
 driver: webdriver = webdriver.Firefox(executable_path=
                                       "C:\\Пользователи\\Данила\\PycharmProjects\\most_popular_words\\fierfoxdriver\\geckodriver-0.33.0")
 
+options: webdriver = webdriver.FirefoxOptions()
+options.add_argument()
+
 try:
-    driver.get(url="https://youtube.com")
-    time.sleep(10)
+    driver.get(url=url)
+    time.sleep(100)
 except Exception as ex:
     print(ex)
 finally:
