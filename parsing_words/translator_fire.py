4799529912d66fcf80c3de3e486e568106c93a50
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-06-25 20:15:53 +0500

    add set word

--- parsing_words/translator_fire.py
+++ parsing_words/translator_fire.py
@@ -1,17 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
+import random
 
 
-proxy = "41.76.145.18:3128"
-firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
-firefox_capabilities['marionette'] = True
-firefox_capabilities["proxy"] = {
-    "proxyType": "MANUAL",
-    "httpProxy": proxy,
-    "sslProxy": proxy
-}
+proxy = random.choice([None, "190.185.109.193:9417"])
 
-url = "https://2ip.ru"
+if proxy:
+    firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+    firefox_capabilities["marionette"] = True
+
+    firefox_capabilities["proxy"] = {
+        "proxyType": "MANUAL",
+        "httpProxy": proxy,
+        "sslProxy": proxy
+    }
+
+url = "https://translate.google.com/?hl=ru&sl=en&tl=ru&op=translate"
 driver: webdriver = webdriver.Firefox(
     executable_path="/fierfoxdriver/geckodriver-0.33.0",
     proxy=proxy
@@ -19,6 +24,16 @@ driver: webdriver = webdriver.Firefox(
 
 try:
     driver.get(url=url)
+    time.sleep(2)
+
+    input_world = driver.find_element(By.CLASS_NAME, "er8xn")
+    input_world.clear()
+    input_world.send_keys("world")
     time.sleep(10)
+
 except Exception as ex:
     print(ex)
+finally:
+    driver.close()
+    driver.quit()
+
