ad8b388f952d8dad763e92333272c4cf39415049
nasdavik <sew542@mail.ru>
2023-06-26 17:59:18 +0500

    Edit parser for chrome

--- parsing_words/translator_ch.py
+++ parsing_words/translator_ch.py
@@ -1,18 +1,29 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
-from fake_useragent import UserAgent
 
+options = webdriver.ChromeOptions()
+options.add_argument("--disable-blink-features=AutomationControlled")
 
-useragent: UserAgent = UserAgent()
-options: webdriver = webdriver.ChromeOptions()
-options.add_argument(f"user-agent={useragent.random}")
-url = "https://www.whatismybrowser.com/detect/what-is-my-user-agent/"
-driver: webdriver = webdriver.Chrome(executable_path="/chromdriver/oldversion/chromedriver.exe",
-                                     options=options)
+url = "https://translate.google.com/?hl=ru&sl=en&tl=ru&op=translate"
+driver: webdriver = webdriver.Chrome(
+    executable_path="/fierfoxdriver/geckodriver-0.33.0",
+    options=options
+)
 
 try:
     driver.get(url=url)
-    time.sleep(10)
+    time.sleep(2)
+
+    input_world = driver.find_element(By.CLASS_NAME, "er8xn")
+    input_world.clear()
+    input_world.send_keys("dog")
+    time.sleep(2)
+
+    get_world = driver.find_element(By.CLASS_NAME, "j7bWb")
+    print(get_world.text)
+    time.sleep(2)
+
 except Exception as ex:
     print(ex)
 finally:
