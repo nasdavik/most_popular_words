2391aafe7c9980df908d637dbbb134b357f8b3ec
nasdavik <sew542@mail.ru>
2023-06-28 18:16:58 +0500

    finally taked translation of word

--- parsing_words/translator_ch.py
+++ parsing_words/translator_ch.py
@@ -17,11 +17,22 @@ try:
 
     input_word = driver.find_element(By.CLASS_NAME, "er8xn")
     input_word.clear()
-    input_word.send_keys("dog")
+    input_word.send_keys("bonus")
     time.sleep(2)
 
-    get_word = driver.find_element(By.XPATH, "/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[2]/c-wiz/div/div/div[1]/div/div[1]/table/tbody[1]/tr[1]")
-    print(get_word.text)
+    get_word = driver.find_elements(By.CLASS_NAME, "TKwHGb")
+    all_translates = {}
+    for word in get_word:
+        if word.text:
+            translator = word.find_element(By.CLASS_NAME, "kgnlhe")
+            usage = word.find_element(By.CLASS_NAME, "YF3enc")
+            if translator.text and usage.get_attribute('aria-label'):
+                all_translates[translator.text] = usage.get_attribute('aria-label')
+    favorite = driver.find_element(By.CLASS_NAME, "ryNqvb")
+    if favorite.text not in all_translates.keys():
+        all_translates[favorite.text] = "Распространенный вариант"
+    for i in all_translates.items():
+        print(i)
     time.sleep(2)
 
 except Exception as ex:
