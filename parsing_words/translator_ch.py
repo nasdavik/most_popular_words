fe05106e66282c96be32067f80fac83b4d7036b4
nasdavik <sew542@mail.ru>
2023-06-30 18:55:50 +0500

    Trying use multiprocessing

--- parsing_words/translator_ch.py
+++ parsing_words/translator_ch.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
+import json
+from multiprocessing import Pool
 
 
 def processing_translate(wrd):
@@ -17,19 +18,19 @@ def processing_translate(wrd):
     try:
         driver.maximize_window()
         driver.get(url=url)
-        time.sleep(3)
+        driver.implicitly_wait(10)
 
         input_word = driver.find_element(By.CLASS_NAME, "er8xn")
         input_word.clear()
         input_word.send_keys(wrd)
-        time.sleep(2)
+        driver.implicitly_wait(10)
 
         get_word = driver.find_elements(By.CLASS_NAME, "TKwHGb")
         all_translates = {}
-        for word in get_word:
-            if word.text:
-                translator = word.find_element(By.CLASS_NAME, "kgnlhe")
-                usage = word.find_element(By.CLASS_NAME, "YF3enc")
+        for point in get_word:
+            if point.text:
+                translator = point.find_element(By.CLASS_NAME, "kgnlhe")
+                usage = point.find_element(By.CLASS_NAME, "YF3enc")
                 if translator.text and usage.get_attribute('aria-label'):
                     all_translates[translator.text] = usage.get_attribute('aria-label')
         favorite = driver.find_element(By.CLASS_NAME, "ryNqvb")
@@ -46,14 +47,21 @@ def processing_translate(wrd):
 
 
 if __name__ == "__main__":
-    info = ["answer", "country", "smile", "bold", "string"]
-    for i in info:
-        def get_answer():
-            return processing_translate(i)
-        answer = get_answer()
-        while len(answer) < 2:
-            answer = get_answer()
-        print(answer)
-        print("#" * 10)
-        print(f"{i} - Completed")
-        print("-" * 10)
+    with open("C:\\Users\\1\\PycharmProjects\\most_popular_words\\words_lib\\words100.json", "r") as json_file:
+        info = json.load(json_file)
+
+        def get_answer(word, attempt=0):
+            ans = processing_translate(word)
+            if attempt == 5:
+                return ans
+            if len(ans) < 2:
+                ans = get_answer(word, attempt=attempt+1)
+            global info
+            info[word] = ans
+            print(info)
+            print(f"{word} - Completed")
+
+        p = Pool(processes=2)
+        p.map(get_answer, info.keys())
+
+# AttributeError: Can't get attribute 'get_answer'
