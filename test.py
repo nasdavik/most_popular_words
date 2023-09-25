2da1e26009a38917ba2321ca13da179c4c30e194
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-09-25 17:11:39 +0500

    finally found the reason why some lists had a random number of words

--- test.py
+++ test.py
@@ -4,33 +4,56 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 import time
 
-options = Options()
-options.set_preference('permissions.default.image', 2)
-driver: webdriver = webdriver.Firefox(
-        executable_path="/fierfoxdriver/geckodriver-0.33.0",
-        options=options
-    )
+# options = Options()
+# options.set_preference('permissions.default.image', 2)
+# driver: webdriver = webdriver.Firefox(
+#         executable_path="/fierfoxdriver/geckodriver-0.33.0",
+#         options=options
+#     )
+#
+# try:
+#     driver.get(url="https://www.kreekly.com/lists/1000-naibolee-populyarnyh-angliyskih-slov/")
+#     driver.implicitly_wait(10)
+#     load = driver.find_element(By.CLASS_NAME, "load_more")
+#     load.click()
+#
+#     time.sleep(10)
+#
+#     words = driver.find_elements(By.CLASS_NAME, "eng")
+#     new_words = [x.text for x in words]
+#     print(len(new_words))
+#     print(new_words)
+#     driver.implicitly_wait(10)
+#
+#
+# except Exception as ex:
+#     print(ex)
+# finally:
+#     driver.close()
+#     driver.quit()
+#
+# ews = 1
+# for i in new_words:
+#     print(ews, i)
+#     ews = ews + 1
+#
+# print(len(set(new_words)))
 
-try:
-    driver.get(url="https://www.kreekly.com/lists/1000-naibolee-populyarnyh-angliyskih-slov/")
-    driver.implicitly_wait(10)
 
-    button = driver.find_element(By.CLASS_NAME, "load_more ")
-    print(button.text)
-    button.click()
-
-    words = driver.find_elements(By.CLASS_NAME, "eng")
-    print(len(words))
-    driver.implicitly_wait(10)
-
-
-except Exception as ex:
-    print(ex)
-finally:
-    driver.close()
-    driver.quit()
+# with open("words_lib/words1000.json", "w", encoding="utf-8") as wr:
+#     all_words = {}
+#     for word in new_words:
+#         all_words[word] = ""
+#     ews = 1
+#     for i in all_words:
+#         print(ews, i)
+#         ews = ews + 1
+#     json.dump(all_words, wr, ensure_ascii=False)
 
 
-with open("words_lib/words1000.json", "r", encoding="utf-8") as read:
-    answer = json.load(read)
-    print(len(answer))
+with open("words_lib/words10000.json", "r", encoding="utf-8") as reading:
+    answer = json.load(reading)
+    ews = 1
+    for i in answer:
+        print(ews, i)
+        ews = ews + 1
