c75a9e502d89dd0076c2459949fbfe3fc0820e27
nasdavik <120183618+nasdavik@users.noreply.github.com>
2023-10-17 20:49:03 +0500

    Finally completed all_words.json

--- test.py
+++ test.py
@@ -4,46 +4,61 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
-#
-# options = Options()
-# options.set_preference('permissions.default.image', 2)
-# driver: webdriver = webdriver.Firefox(
-#         executable_path="/fierfoxdriver/geckodriver-0.33.0",
-#         options=options
-#     )
-#
-# with open("words_lib/words20000.json", encoding="utf-8") as rd:
-#     words = json.load(rd)
-#     for word in words:
-#         try:
-#             driver.get(url="https://www.google.com/")
-#
-#             word_input = driver.find_element(By.ID, "APjFqb")
-#             word_input.clear()
-#             word_input.send_keys(f'\"{word}\"')
-#             word_input.send_keys(Keys.ENTER)
-#
-#             time.sleep(3)
-#
-#             stat = driver.find_element(By.ID, "result-stats")
-#             answer = stat.text.split()
-#             answer = "".join(answer[2:-2])
-#             print(answer)
-#
-#         except Exception as ex:
-#             print(ex)
-#
-# driver.close()
-# driver.quit()
+import random
 
 
-with open("words_lib/words20000.json", "r", encoding="utf-8") as reading:
-    standart = json.load(reading)
-    with open("words_lib/words5000.json") as rd:
-        instance = json.load(rd)
-        for i in instance:
-            try:
-                standart[i]
-            except Exception as ex:
-                print(ex)
-                print(i)
+with open("words_lib/test.json", encoding="utf-8") as rd:
+    words = json.load(rd)
+    for word in words:
+
+        proxy = "38.170.104.137:9144"
+
+        if proxy:
+            firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
+            firefox_capabilities["marionette"] = True
+
+            firefox_capabilities["proxy"] = {
+                "proxyType": "MANUAL",
+                "httpProxy": proxy,
+                "sslProxy": proxy
+            }
+
+        options = Options()
+        options.set_preference('permissions.default.image', 2)
+        driver: webdriver = webdriver.Firefox(
+            executable_path="/fierfoxdriver/geckodriver-0.33.0",
+            options=options, proxy=proxy
+        )
+
+        try:
+            driver.get(url="https://www.google.com/")
+
+            word_input = driver.find_element(By.ID, "APjFqb")
+            word_input.clear()
+            word_input.send_keys(f'\"{word}\"')
+            word_input.send_keys(Keys.ENTER)
+
+            time.sleep(3)
+
+            stat = driver.find_element(By.ID, "result-stats")
+            answer = stat.text.split()
+            answer = "".join(answer[2:-2])
+            words[word] = answer
+            print(f'{word} - complete')
+
+        except Exception as ex:
+            print(ex)
+
+        finally:
+            driver.close()
+            driver.quit()
+
+    with open("words_lib/test.json", "w", encoding="utf-8") as wr:
+        json.dump(words, wr, ensure_ascii=False)
+
+
+# with open("words_lib/all_words.json", encoding="utf-8") as rd:
+#     standart = json.load(rd)
+#     with open("words_lib/test.json", "w", encoding="utf-8") as wr:
+#         json.dump(standart, wr, ensure_ascii=False)
+#
